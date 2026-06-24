768b56266c954134a0cd05cb61d216ed9f21622b
Áreum Tecnologia <dev@areum.com.br>
2026-06-24 18:17:26 -0300

    v1.2.2: implement StatusHandler to manage status updates and mentions, and bump package version to 1.2.1

--- handlers/status.js
+++ handlers/status.js
@@ -1,40 +1,4 @@
 // Classe para manipular envio e gerenciamento de status seguindo padrao de handlers
-// Envia uma mensagem de status e mensiona apos a publicacao
-// const msgRes = await sock.sendMessage(`${channel}@broadcast`,
-//     // { text: 'UnityChat já está disponível. Acesse https://unitychat.areum.com.br e teste grátis por 30 dias!' },
-//     {
-//         image: { url: 'https://unitychat.areum.com.br/assets/img/001.jpg' },
-//         caption: 'UnityChat já está disponível. Acesse https://unitychat.areum.com.br e teste grátis por 30 dias!',
-//     },
-//     {
-//         broadcast: true,
-//         backgroundColor: '#288d7c85',
-//         font: 2,
-//         statusJidList: [
-//             '559187513656@s.whatsapp.net',
-//             // '559181648646@s.whatsapp.net',
-//             // '559184185577@s.whatsapp.net',
-//         ]
-//     });
-// console.log(msgRes);
-// const remoteJid = '120363421363321743@g.us';
-// const rs = await sock.sendMessage(remoteJid, { text: 'hello word' }, { quoted: msgRes });
-// 
-// Envia uma mensagem de status e mensiona na mesma publicacao
-// const jidat = [
-//                 '120363421363321743@g.us',
-//                 '559184185577@s.whatsapp.net'
-//             ]
-//             // Text
-//             const x = await sock.sendStatusMentions(
-//                 {
-//                     text: 'Hello Everyone :3',
-//                     font: 2, // optional
-//                     textColor: 'FF0000', // optional
-//                     backgroundColor: '#000000' // optional
-//                 },
-//                 jidat // Limit to 5 mentions per status
-//             );
 
 class StatusHandler {
     constructor(client) {
@@ -69,3 +33,5 @@ class StatusHandler {
     }
 
 }
+
+module.exports = StatusHandler;
